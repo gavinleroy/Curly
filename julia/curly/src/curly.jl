cd014d398f0bd452fe5ab80add5d0713d6600a93
gavinleroy <gavinleroy6@gmail.com>
2021-08-18 16:07:53 -0600

    updated Curly implementation/tests

--- julia/curly/src/curly.jl
+++ julia/curly/src/curly.jl
@@ -70,81 +70,88 @@ end
 # PARSER #
 ##########
 
-ws = p"[\s\n]*"
+ws = Drop(Star(Space()))
 
-opt_ws = ws | e""
+@with_pre ws begin
 
-expr = Delayed()
+    curly_int = p"\d+" > (x -> CInt(parse(Int64, x)))
 
-curly_int = p"\d+" > (x -> CInt(parse(Int64, x)))
+    sym = p"[a-zA-Z]+[a-zA-Z_]*" > CSymb
 
-sym = p"[a-zA-Z]+[a-zA-Z_]*" > CSymb
+    # Expressions
 
-plus = E"{+ " + ~opt_ws + expr + ~opt_ws + expr + ~opt_ws + E"}" > Plus
+    expr = Delayed()
 
-sub = E"{- " + ~opt_ws +  expr + ~opt_ws + expr + ~opt_ws + E"}" > Sub
+    cexpr = Delayed()
 
-ifzero = E"{if0 " + ~opt_ws + expr + ~opt_ws + expr + ~opt_ws +  expr + ~opt_ws + E"}" > IfZero
+    group = E"{" + ws + expr + ws + E"}"
 
-lete = E"{let {" + ~opt_ws + expr + ~opt_ws + expr + ~opt_ws + E"}" + ~opt_ws + expr + ~opt_ws + E"}" > Let
+    plus = E"+" + cexpr + cexpr > Plus
 
-lambda = E"{lambda {" + ~opt_ws + expr + ~opt_ws + E"}" + ~opt_ws + expr + ~opt_ws + E"}" > Lambda
+    sub = E"-" +  cexpr + cexpr > Sub
 
-app = E"{" + ~opt_ws + expr + ~opt_ws + expr + ~opt_ws + E"}" > App
+    ifzero = E"if0" + cexpr + cexpr +  cexpr > IfZero
 
-expr.matcher = curly_int | sym | plus | sub | ifzero | lete | lambda | app
+    lete = E"let" + ws + E"{" + sym + cexpr + E"}" + cexpr > Let
 
-prog = expr + Eos()
+    lambda = E"lambda" + ws + E"{" + sym + ws + E"}" + cexpr > Lambda
 
-##########
-# INTERP #
-##########
+    app = cexpr + cexpr > App
 
-interp(n::Node) = println("Interp is undefined for this construct")
+    expr.matcher = plus | sub | ifzero | lete | lambda | app
 
-interp(n::CInt) = :($n.val)
+    cexpr.matcher = (group | curly_int | sym) + ws
 
-interp(s::CSymb) = Symbol(s.sym)
+    prog = cexpr + ws + Eos() # Add `Eos` to ensure that all input is matched.
 
-function interp(p::Plus)
-    l = interp(p.lhs);
-    r = interp(p.rhs);
+end
+
+#############
+# TRANSFORM #
+#############
+
+# Any other input to transform besides the below should be an error.
+transform(n) = @assert false
+
+transform(n::CInt) = n.val
+
+transform(s::CSymb) = Symbol(s.sym)
+
+function transform(p::Plus)
+    l = transform(p.lhs);
+    r = transform(p.rhs);
     return :($l + $r)
 end
 
-function interp(s::Sub)
-    l = interp(s.lhs);
-    r = interp(s.rhs);
+function transform(s::Sub)
+    l = transform(s.lhs);
+    r = transform(s.rhs);
     return :($l - $r)
 end
 
-function interp(iz::IfZero)
-    cnd = interp(iz.cnd);
-    te = interp(iz.true_e);
-    fe = interp(iz.false_e);
+function transform(iz::IfZero)
+    cnd = transform(iz.cnd);
+    te = transform(iz.true_e);
+    fe = transform(iz.false_e);
     return :(($cnd == 0) ? $te : $fe)
 end
 
-function interp(l::Let)
-    sy = interp(l.arg);
-    v = interp(l.val);
-    bdy = interp(l.bdy);
-    # Would be better to exchange for a typechecker
-    @assert typeof(sy) == Symbol;
+function transform(l::Let)
+    sy = transform(l.arg);
+    v = transform(l.val);
+    bdy = transform(l.bdy);
     return :( ($sy -> $bdy)($v) )
 end
 
-function interp(lam::Lambda)
-    sy = interp(lam.arg);
-    arg_ex = interp(lam.bdy);
-    # Would be better to exchange for a typechecker
-    @assert typeof(sy) == Symbol
+function transform(lam::Lambda)
+    sy = transform(lam.arg);
+    arg_ex = transform(lam.bdy);
     return :(($sy -> $arg_ex))
 end
 
-function interp(a::App)
-    g = interp(a.f);
-    p = interp(a.arg);
+function transform(a::App)
+    g = transform(a.f);
+    p = transform(a.arg);
     return :($g($p))
 end
 
@@ -153,36 +160,228 @@ end
 ########
 
 "Curly program string"
-macro curly_str(line)
-    # parse one program (i.e. expr)
+macro curly_str(source)
+    # parse one program (i.e. cexpr)
     # and then take the first result
     # as there should only be one :)
-    curly_expr = parse_one(strip(line), prog)[1];
-    julia_expr = interp(curly_expr);
-    return julia_expr
+    curly_expr = parse_one(source, prog)[1];
+    return transform(curly_expr)
 end
 
-"Macro for pretty-printing test cases"
+"Macro for pretty-printing test results"
 macro test(prog, expected)
     quote
         if $prog == $expected
-            printstyled("[Pass] \U2713 \n", color=:green);
+            printstyled("[Pass] \U2713 \n", color=:green)
         else
-            printstyled("[Failed] ==> "
+            printstyled("[Fail] \U0021 \n"
                         , $(Meta.quot(prog))
                         , " == "
                         , $(Meta.quot(expected)), "\n"
-                        , color=:red);
+                        , " Received : ", $prog
+                        , color=:red)
         end
     end
 end
 
-"Run the Curly testing suite"
+"Run the Curly test suite"
 function run_tests()
+    # Basic additive properties
     @test curly"{+ 1 2}" 3;
     @test curly"{+ 2 1}" 3;
+    @test curly"{+ 1 {+ 2 3}}" 6;
+    @test curly"{+ {+ 1 2} 3}" 6;
     @test curly"{+ 1 0}" 1;
     @test curly"{+ 0 1}" 1;
+
+    # subtractive properties
+    @test curly"{- 42 0}" 42;
+    @test curly"{- 0 42}" -42;
+
+    # Handle arbitrary whitespace
+    @test curly"""
+          {+ 1
+            {- 0
+              {+ 0
+                1}}}
+          """ 0
+
+    # simple lambdas and application
+    @test curly"{{lambda {x} x} 6}" 6;
+    @test curly"{{lambda {x} {+ x 1}} 6}" 7;
+    @test curly"""
+          {{{lambda {x}
+            {lambda {y}
+              {+ x y}}}
+              7} 4}
+          """ 11
+
+    @test curly"""
+          {let
+{f {
+lambda {
+y} {lambda {x}
+                        {
++ x
+y}}}} {-
+{ {     f 10
+} 20} {{f 30} 40}}}
+            """ -40
+    @test curly"""
+    {
+    let
+    {
+    x
+    1
+    }
+    {
+    let
+    {
+    f
+    {
+    lambda
+    {
+    x
+    }
+    {
+    -
+    x
+    1
+    }
+    }
+    }
+    {
+    +
+    x
+    {
+    f
+    x
+    }
+    }
+    }
+    }
+    """ 1
+
+    # Testing Recursive programs
+    @test curly"""
+          {let {Y {lambda {f}
+                    {{lambda {x}
+                      {x x}}
+                      {lambda {x}
+                        {f {lambda {y}
+                          {{x x} y}}}}}}}
+            {let {sum {lambda {f}
+                        {lambda {x}
+                          {if0 x
+                            0
+                            {+ x {f {- x 1}}}}}}}
+              {{Y sum} 10}}}
+          """ 55
+
+    @test curly"""
+          {let {Y {lambda {f}
+                    {{lambda {x}
+                      {x x}}
+                      {lambda {x}
+                        {f {lambda {y}
+                          {{x x} y}}}}}}}
+            {let {sum {lambda {f}
+                        {lambda {x}
+                          {if0 x
+                            0
+                            {+ x {f {- x 1}}}}}}}
+              {{Y sum} 200000}}}
+          """ 20000100000
+
+    @test curly"""
+          {let {Y {lambda {f}
+                    {{lambda {x}
+                      {x x}}
+                      {lambda {x}
+                        {f {lambda {y}
+                          {{x x} y}}}}}}}
+            {let {mult {lambda {f}
+                        {lambda {x}
+                          {lambda {y}
+                            {if0 y
+                              0
+                              {+ x {{f x } {- y 1}}}}}}}}
+              {{{Y mult} 7} 6}}}
+          """ 42
+
+    @test curly"""
+          {let {Y {lambda {f}
+                    {{lambda {x}
+                      {x x}}
+                      {lambda {x}
+                        {f {lambda {y}
+                          {{x x} y}}}}}}}
+            {let {mult {Y {lambda {f}
+                        {lambda {x}
+                          {lambda {y}
+                            {if0 y
+                              0
+                              {+ x {{f x } {- y 1}}}}}}}}}
+              {-
+                {{mult 7} {+ 4 3}}
+                {+ {{mult 7} 4} {{mult 3} 7}}}}}
+          """ 0
+
+    @test curly"""
+          {let {Y {lambda {f}
+                    {{lambda {x}
+                      {x x}}
+                      {lambda {x}
+                        {f {lambda {y}
+                          {{x x} y}}}}}}}
+            {let {mult {lambda {f}
+                        {lambda {x}
+                          {lambda {y}
+                            {if0 y
+                              0
+                              {+ x {{f x } {- y 1}}}}}}}}
+              {{{Y mult} 73} 648}}}
+          """ 47304
+
+    # the recursive factorial can easily cause a stack overflow
+    # due to the also recursive multiplication.
+    @test curly"""
+          {let {Y {lambda {f}
+                    {{lambda {x}
+                      {x x}}
+                      {lambda {x}
+                        {f {lambda {y}
+                          {{x x} y}}}}}}}
+            {let {mult {Y {lambda {f}
+                        {lambda {x}
+                          {lambda {y}
+                            {if0 y
+                              0
+                              {+ x {{f x } {- y 1}}}}}}}}}
+              {let {fac {Y {lambda {f}
+                            {lambda {x}
+                              {if0 x
+                                1
+                                {{mult x} {f {- x 1}}}}}}}}
+                {fac 5}}}}
+          """ 120
+
+    @test curly"""
+          {let {Y {lambda {f}
+                    {{lambda {x}
+                      {x x}}
+                      {lambda {x}
+                        {f {lambda {y}
+                          {{x x} y}}}}}}}
+            {let {fib {Y {lambda {f}
+                          {lambda {x}
+                            {if0 {- x 1}
+                              1
+                              {if0 x
+                                1
+                                {+ {f {- x 1}} {f {- x 2}}}}}}}}}
+              {fib 42}}}
+          """ 433494437
 end
 
 end # module curly
